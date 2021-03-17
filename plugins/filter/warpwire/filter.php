a822c33c12025f10ed67dba87b29f10f8e519aa6
Joel Feiner <joel@warpwire.net>
2021-03-17 18:15:58 -0400

    Pass along additional context fields in LTI requests

--- plugins/filter/warpwire/filter.php
+++ plugins/filter/warpwire/filter.php
@@ -16,130 +16,158 @@
 
 defined('MOODLE_INTERNAL') || die('Invalid access');
 
-class filter_warpwire extends moodle_text_filter {
+class filter_warpwire extends moodle_text_filter
+{
+    public function filter($text, array $options = array())
+    {
+        global $COURSE, $PAGE, $CFG;
 
-  public function filter($text, array $options = array()) {
+        // iframe template element
+        $iframe_template = '<iframe 
+          width="WIDTH"
+          height="HEIGHT"
+          src="URL"
+          frameborder="0"
+          scrolling="0"
+          allow="autoplay *; encrypted-media *; fullscreen *;"
+          title="Warpwire Media"
+          allowfullscreen></iframe>';
 
-    global $COURSE, $CFG;
+        // collect information about context to send to iframe
+        $modInfo = get_fast_modinfo($COURSE);
+        $sections = $modInfo->get_section_info_all();
 
-    // iframe template element
-    $iframe_template = '<iframe 
-    	width="WIDTH"
-      height="HEIGHT"
-    	src="URL"
-      frameborder="0"
-      scrolling="0"
-      allow="autoplay *; encrypted-media *; fullscreen *;"
-      title="Warpwire Media"
-      allowfullscreen></iframe>';
+        // match all warpwire shortcode instances returned from plugins
+        if (preg_match_all('/<img.*?>/is', $text, $matches_code)) {
+            foreach ($matches_code[0] as $ci => $code) {
+                $textToReplace = $code;
 
-    // match all warpwire shortcode instances returned from plugins
-    if (preg_match_all('/<img.*?>/is', $text, $matches_code)) {
-      foreach ($matches_code[0] as $ci => $code) {
+                if (preg_match('/\[warpwire:(.*)?\]/is', urldecode($code), $matches_string)) {
+                    $url = htmlspecialchars_decode($matches_string[1]);
 
-        $textToReplace = $code;
+                    $currentSectionId = null;
+                    foreach ($sections as $section) {
+                        $sectionContent = $section->summary;
+                        if (strstr($sectionContent, urlencode($url)) !== false) {
+                            $currentSectionId = $section->id;
+                            break;
+                        }
+                    }
 
-        if (preg_match('/\[warpwire:(.*)?\]/is', urldecode($code), $matches_string)) {
+                    // default width and height values for iframe
+                    $iframe_width = 480;
+                    $iframe_height = 360;
 
-          $url = htmlspecialchars_decode($matches_string[1]);
+                    $url_parts = parse_url($url);
 
-          // default width and height values for iframe
-          $iframe_width = 480;
-          $iframe_height = 360;
+                    $parameters = array();
+                    if (!empty($url_parts['query'])) {
+                        parse_str($url_parts['query'], $parameters);
+                    }
 
-          $url_parts = parse_url($url);
+                    $url_parts['query'] = http_build_query($parameters, '', '&');
 
-          $parameters = array();
-          if(!empty($url_parts['query']))
-            parse_str($url_parts['query'], $parameters);
+                    $url = $url_parts['scheme'].'://'.$url_parts['host'].$url_parts['path'].'?'.$url_parts['query'];
 
-          $url_parts['query'] = http_build_query($parameters, '', '&');
+                    $parts = array(
+                        'url' => $url,
+                        'course_id' => $COURSE->id,
+                        'module_id' => isset($PAGE->cm->id) ? $PAGE->cm->id : '',
+                        'section_id' => $currentSectionId
+                    );
 
-          $url = $url_parts['scheme'].'://'.$url_parts['host'].$url_parts['path'].'?'.$url_parts['query'];
+                    $partsString = http_build_query($parts, '', '&');
 
-          $parts = array(
-            'url' => $url,
-            'course_id' => $COURSE->id
-          );
+                    // TODO: edit here
 
-          $partsString = http_build_query($parts, '', '&');
+                    $url = $CFG->wwwroot . '/local/warpwire/?' .$partsString;
 
-          // TODO: edit here
+                    if (!empty($parameters['width'])) {
+                        $iframe_width = $parameters['width'];
+                    }
+                    if (!empty($parameters['height'])) {
+                        $iframe_height = $parameters['height'];
+                    }
 
-          $url = $CFG->wwwroot . '/local/warpwire/?' .$partsString;
+                    if (class_exists('DOMDocument')) {
+                        $doc = new DOMDocument();
+                        $doc->loadHTML($code);
+                        $imageTags = $doc->getElementsByTagName('img');
 
-          if(!empty($parameters['width']))
-            $iframe_width = $parameters['width'];
-          if(!empty($parameters['height']))
-            $iframe_height = $parameters['height'];       
-
-          if(class_exists('DOMDocument')){
-            $doc = new DOMDocument();
-            $doc->loadHTML($code);
-            $imageTags = $doc->getElementsByTagName('img');
-
-            foreach($imageTags as $tag) {
-              $iframe_width = $tag->getAttribute('width');
-              $iframe_height = $tag->getAttribute('height');
-            }
-          }
+                        foreach ($imageTags as $tag) {
+                            $iframe_width = $tag->getAttribute('width');
+                            $iframe_height = $tag->getAttribute('height');
+                        }
+                    }
             
-          $patterns = array('/URL/', '/WIDTH/', '/HEIGHT/');
-          $replace = array($url, $iframe_width, $iframe_height);
-          $iframe_html = preg_replace($patterns, $replace, $iframe_template);
+                    $patterns = array('/URL/', '/WIDTH/', '/HEIGHT/');
+                    $replace = array($url, $iframe_width, $iframe_height);
+                    $iframe_html = preg_replace($patterns, $replace, $iframe_template);
 
-          // replace the shortcode with the iframe html
-          $text = str_replace($textToReplace, $iframe_html, $text);
-        }
-      }
-    }
-
-    // match all warpwire shortcode instances manually inserted
-    if (preg_match_all('/\[warpwire(\:(.+))?( .+)?\](.+)?\/a>/isU', $text, $matches_code)) {
-      foreach ($matches_code[0] as $index => $code) {
-
-        $textToReplace = $matches_code[0][$index];
-
-        $url = '';
-        if (!empty($matches_code[3][$index])) {
-          $url = preg_replace('/^ href=("|\')/','',$matches_code[3][$index]);
+                    // replace the shortcode with the iframe html
+                    $text = str_replace($textToReplace, $iframe_html, $text);
+                }
+            }
         }
 
-        $url = htmlspecialchars_decode($url);
+        // match all warpwire shortcode instances manually inserted
+        if (preg_match_all('/\[warpwire(\:(.+))?( .+)?\](.+)?\/a>/isU', $text, $matches_code)) {
+            foreach ($matches_code[0] as $index => $code) {
+                $textToReplace = $matches_code[0][$index];
 
-        // default width and height values for iframe
-        $iframe_width = 480;
-        $iframe_height = 360;
+                $url = '';
+                if (!empty($matches_code[3][$index])) {
+                    $url = preg_replace('/^ href=("|\')/', '', $matches_code[3][$index]);
+                }
 
-        $url_parts['query'] = http_build_query($parameters, '', '&');
+                $url = htmlspecialchars_decode($url);
 
-        $url = $url_parts['scheme'].'://'.$url_parts['host'].$url_parts['path'].'?'.$url_parts['query'];
+                $currentSectionId = null;
+                foreach ($sections as $section) {
+                    $sectionContent = $section->summary;
+                    if (strstr($sectionContent, urlencode($url)) !== false) {
+                        $currentSectionId = $section->id;
+                        break;
+                    }
+                }
 
-        $parts = array(
-          'url' => $url,
-          'course_id' => $COURSE->id
-        );
+                // default width and height values for iframe
+                $iframe_width = 480;
+                $iframe_height = 360;
 
-        $partsString = http_build_query($parts, '', '&');
+                $url_parts['query'] = http_build_query($parameters, '', '&');
 
-        // TODO: edit here
+                $url = $url_parts['scheme'].'://'.$url_parts['host'].$url_parts['path'].'?'.$url_parts['query'];
 
-        $url = $CFG->wwwroot . '/local/warpwire/?' .$partsString;
+                $parts = array(
+                    'url' => $url,
+                    'course_id' => $COURSE->id,
+                    'module_id' => isset($PAGE->cm->id) ? $PAGE->cm->id : '',
+                    'section_id' => $currentSectionId
+                );
 
-        if(!empty($parameters['width']))
-          $iframe_width = $parameters['width'];
-        if(!empty($parameters['height']))
-          $iframe_height = $parameters['height'];        
+                $partsString = http_build_query($parts, '', '&');
 
-        $patterns = array('/URL/', '/WIDTH/', '/HEIGHT/');
-        $replace = array($url, $iframe_width, $iframe_height);
-        $iframe_html = preg_replace($patterns, $replace, $iframe_template);
+                // TODO: edit here
 
-        // replace the shortcode with the iframe html
-        $text = str_replace($textToReplace, $iframe_html, $text);
-      }
+                $url = $CFG->wwwroot . '/local/warpwire/?' .$partsString;
+
+                if (!empty($parameters['width'])) {
+                    $iframe_width = $parameters['width'];
+                }
+                if (!empty($parameters['height'])) {
+                    $iframe_height = $parameters['height'];
+                }
+
+                $patterns = array('/URL/', '/WIDTH/', '/HEIGHT/');
+                $replace = array($url, $iframe_width, $iframe_height);
+                $iframe_html = preg_replace($patterns, $replace, $iframe_template);
+
+                // replace the shortcode with the iframe html
+                $text = str_replace($textToReplace, $iframe_html, $text);
+            }
+        }
+
+        return $text;
     }
-
-    return $text;
-  }
 }
--- plugins/local/warpwire/index.php
+++ plugins/local/warpwire/index.php
@@ -14,44 +14,47 @@ require_login();
 global $USER, $COURSE;
 
 $course = $DB->get_record('course', array('id' => $_GET['course_id']));
+$sectionId = isset($_GET['section_id']) ? $_GET['section_id'] : '';
+$moduleId = isset($_GET['module_id']) ? $_GET['module_id'] : '';
 
-warpwire_external_content($USER, $course);
+warpwire_external_content($USER, $course, $sectionId, $moduleId);
 exit;
 
 
-function warpwire_external_content($user, $course) {
-  global $CFG;
+function warpwire_external_content($user, $course, $sectionId, $moduleId)
+{
+    global $CFG;
 
-  $lti_url_parts = parse_url(get_config('local_warpwire', 'warpwire_lti'));
-  $url_parts = parse_url($_GET['url']);
+    $lti_url_parts = parse_url(get_config('local_warpwire', 'warpwire_lti'));
+    $url_parts = parse_url($_GET['url']);
 
-  $host_match = false;
+    $host_match = false;
 
-  // the allowed url list
-  $host_urls = array(
+    // the allowed url list
+    $host_urls = array(
     'warpwire.com',
     $lti_url_parts['host']
   );
 
-  // iterate through valid host urls, and set match if found
-  foreach($host_urls as $host_url) {
-    if (strpos(strtolower($url_parts['host']), strtolower($host_url)) !== false) {
-      $host_match = true;
-      break;
+    // iterate through valid host urls, and set match if found
+    foreach ($host_urls as $host_url) {
+        if (strpos(strtolower($url_parts['host']), strtolower($host_url)) !== false) {
+            $host_match = true;
+            break;
+        }
     }
-  }
 
-  // host is not found in the allowed list - redirect to the provided url
-  if(!$host_match) {
-    header('Location: '.$_GET['url']);
-    exit;
-  }  
+    // host is not found in the allowed list - redirect to the provided url
+    if (!$host_match) {
+        header('Location: '.$_GET['url']);
+        exit;
+    }
 
-  // user roles
-  $roles = lti_get_ims_role($user, null, $course->id, false);
+    // user roles
+    $roles = lti_get_ims_role($user, null, $course->id, false);
 
-  // LTI parameters
-  $params = array(
+    // LTI parameters
+    $params = array(
     'oauth_version' => '1.0',
     'oauth_nonce' => md5(mt_rand()),
     'oauth_timestamp' => time() + 600,
@@ -62,61 +65,63 @@ function warpwire_external_content($user, $course) {
     'context_id' => $course->id,
     'context_label' => $course->shortname,
   );
-  if ($course->format == 'site') {
-    $parms['context_type'] = 'Group';
-  } else {
-    $params['context_type'] = 'CourseSection';
+    if ($course->format == 'site') {
+        $parms['context_type'] = 'Group';
+    } else {
+        $params['context_type'] = 'CourseSection';
+        $params['lis_course_section_sourcedid'] = $course->idnumber;
+    }
+
     $params['lis_course_section_sourcedid'] = $course->idnumber;
-  }
+    $params['lis_person_name_given'] = $user->firstname;
+    $params['lis_person_name_family'] = $user->lastname;
+    $params['lis_person_name_full'] = fullname($user);
+    $params['ext_user_username'] = $user->username;
+    $params['lis_person_contact_email_primary'] = $user->email;
+    $params['launch_presentation_locale'] = current_language();
+    $params['ext_lms'] = 'moodle-2';
+    $params['tool_consumer_info_product_family_code'] = 'moodle';
+    $params['tool_consumer_info_version'] = strval($CFG->version);
+    // Add oauth_callback to be compliant with the 1.0A spec.
+    $params['oauth_callback'] = 'about:blank';
+    $params['lti_version'] = 'LTI-1p0';
+    $params['lti_message_type'] = 'ContentItemSelectionRequest';
 
-  $params['lis_course_section_sourcedid'] = $course->idnumber;
-  $params['lis_person_name_given'] = $user->firstname;
-  $params['lis_person_name_family'] = $user->lastname;
-  $params['lis_person_name_full'] = fullname($user); 
-  $params['ext_user_username'] = $user->username;
-  $params['lis_person_contact_email_primary'] = $user->email;
-  $params['launch_presentation_locale'] = current_language();
-  $params['ext_lms'] = 'moodle-2';
-  $params['tool_consumer_info_product_family_code'] = 'moodle';
-  $params['tool_consumer_info_version'] = strval($CFG->version);
-  // Add oauth_callback to be compliant with the 1.0A spec.
-  $params['oauth_callback'] = 'about:blank';  
-  $params['lti_version'] = 'LTI-1p0';
-  $params['lti_message_type'] = 'ContentItemSelectionRequest';
+    if (!empty($CFG->mod_lti_institution_name)) {
+        $params['tool_consumer_instance_name'] = trim(html_to_text($CFG->mod_lti_institution_name, 0));
+    } else {
+        $params['tool_consumer_instance_name'] = get_site()->shortname;
+    }
 
-  if (!empty($CFG->mod_lti_institution_name)) {
-    $params['tool_consumer_instance_name'] = trim(html_to_text($CFG->mod_lti_institution_name, 0));
-  } else {
-    $params['tool_consumer_instance_name'] = get_site()->shortname;
-  }
+    $params['tool_consumer_instance_description'] = trim(html_to_text(get_site()->fullname, 0));
+    $params['launch_presentation_return_url'] = $CFG->wwwroot . '/local/warpwire/html/warpwire.html';
+    $params['oauth_signature_method'] = 'HMAC-SHA1';
+    $params['custom_context_id'] = $course->id;
+    $params['custom_section_id'] = $sectionId;
+    $params['custom_module_id'] = $moduleId;
+    $params['custom_plugin_info'] = '';
 
-  $params['tool_consumer_instance_description'] = trim(html_to_text(get_site()->fullname, 0));
-  $params['launch_presentation_return_url'] = $CFG->wwwroot . '/local/warpwire/html/warpwire.html';
-  $params['oauth_signature_method'] = 'HMAC-SHA1';
-  $params['custom_context_id'] = $course->id;
-  $params['custom_plugin_info'] = '';
-
-  if( ($lti_url_parts['host'] != $url_parts['host'])
+    if (($lti_url_parts['host'] != $url_parts['host'])
     || ($lti_url_parts['path'] != $url_parts['path'])
     || (!empty($url_parts['query']))
   ) {
-    $params['returnContext'] = $_GET['url'];
-  }
+        $params['returnContext'] = $_GET['url'];
+    }
   
-  // build the OAuth signature
-  $sig = build_signature('POST', get_config('local_warpwire', 'warpwire_lti'), $params, get_config('local_warpwire', 'warpwire_secret'));
+    // build the OAuth signature
+    $sig = build_signature('POST', get_config('local_warpwire', 'warpwire_lti'), $params, get_config('local_warpwire', 'warpwire_secret'));
 
-  $params['oauth_signature'] = $sig;
+    $params['oauth_signature'] = $sig;
 
-  // build the form to submit LTI credentials
-  $content = '<html><head></head><body><form id="warpwire_lti_post" method="POST" enctype="application/x-www-form-urlencoded" action="'.get_config('local_warpwire', 'warpwire_lti').'">'.PHP_EOL;
-  foreach($params AS $key => $value) {
-    $content .= '<input type="hidden" name="'.$key.'" value="'.$value.'" />';
-  }
-  $content .= '<div id="warpwire_display_submit"><p>Please press the Submit button to continue.</p>';
-  $content .= '<p><input type="submit" value="Submit"></p></div>';
-  $content .= '</form>';
-  $content .= '
+    // build the form to submit LTI credentials
+    $content = '<html><head></head><body><form id="warpwire_lti_post" method="POST" enctype="application/x-www-form-urlencoded" action="'.get_config('local_warpwire', 'warpwire_lti').'">'.PHP_EOL;
+    foreach ($params as $key => $value) {
+        $content .= '<input type="hidden" name="'.$key.'" value="'.$value.'" />';
+    }
+    $content .= '<div id="warpwire_display_submit"><p>Please press the Submit button to continue.</p>';
+    $content .= '<p><input type="submit" value="Submit"></p></div>';
+    $content .= '</form>';
+    $content .= '
     <script>
       (function(){
         var warpwireDisplaySection = document.getElementById("warpwire_display_submit");
@@ -137,86 +142,91 @@ function warpwire_external_content($user, $course) {
       })();
     </script>';
 
-  $content .= '</body></html>';
+    $content .= '</body></html>';
 
-  echo($content);
-  exit;
+    echo($content);
+    exit;
 }
 
-function build_signature($method, $url, $params, $secret) {
+function build_signature($method, $url, $params, $secret)
+{
 
   // parse the provided url to be normalized
-  $url_parts = parse_url($url);
-  $normalized_url = $url_parts['scheme'] . "://" . $url_parts['host'] . $url_parts['path'];
+    $url_parts = parse_url($url);
+    $normalized_url = $url_parts['scheme'] . "://" . $url_parts['host'] . $url_parts['path'];
 
-  // Remove oauth_signature if present
-  // Ref: Spec: 9.1.1 ("The oauth_signature parameter MUST be excluded.")
-  if (isset($params['oauth_signature'])) {
-    unset($params['oauth_signature']);
-  }
+    // Remove oauth_signature if present
+    // Ref: Spec: 9.1.1 ("The oauth_signature parameter MUST be excluded.")
+    if (isset($params['oauth_signature'])) {
+        unset($params['oauth_signature']);
+    }
 
-  $signable_params = build_http_query($params);
+    $signable_params = build_http_query($params);
 
-  $parts = array(
+    $parts = array(
     $method,
     $normalized_url,
     $signable_params
   );
 
-  $base_string = implode('&', urlencode_rfc3986($parts));
+    $base_string = implode('&', urlencode_rfc3986($parts));
 
-  $key_parts = array(
+    $key_parts = array(
     $secret,
     ""
   );
 
-  $key_parts = urlencode_rfc3986($key_parts);
-  $key = implode('&', $key_parts);
+    $key_parts = urlencode_rfc3986($key_parts);
+    $key = implode('&', $key_parts);
 
-  $computed_signature = base64_encode(hash_hmac('sha1', $base_string, $key, true));
-  return $computed_signature;
+    $computed_signature = base64_encode(hash_hmac('sha1', $base_string, $key, true));
+    return $computed_signature;
 }
 
-function build_http_query($params) {
-  if (!$params) return '';
-
-  // Urlencode both keys and values
-  $keys = urlencode_rfc3986(array_keys($params));
-  $values = urlencode_rfc3986(array_values($params));
-  $params = array_combine($keys, $values);
-
-  // Parameters are sorted by name, using lexicographical byte value ordering.
-  // Ref: Spec: 9.1.1 (1)
-  uksort($params, 'strcmp');
-
-  $pairs = array();
-  foreach ($params as $parameter => $value) {
-    if (is_array($value)) {
-      // If two or more parameters share the same name, they are sorted by their value
-      // Ref: Spec: 9.1.1 (1)
-      natsort($value);
-      foreach ($value as $duplicate_value) {
-        $pairs[] = $parameter . '=' . $duplicate_value;
-      }
-    } else {
-      $pairs[] = $parameter . '=' . $value;
+function build_http_query($params)
+{
+    if (!$params) {
+        return '';
     }
-  }
-  // For each parameter, the name is separated from the corresponding value by an '=' character (ASCII code 61)
-  // Each name-value pair is separated by an '&' character (ASCII code 38)
-  return implode('&', $pairs);
+
+    // Urlencode both keys and values
+    $keys = urlencode_rfc3986(array_keys($params));
+    $values = urlencode_rfc3986(array_values($params));
+    $params = array_combine($keys, $values);
+
+    // Parameters are sorted by name, using lexicographical byte value ordering.
+    // Ref: Spec: 9.1.1 (1)
+    uksort($params, 'strcmp');
+
+    $pairs = array();
+    foreach ($params as $parameter => $value) {
+        if (is_array($value)) {
+            // If two or more parameters share the same name, they are sorted by their value
+            // Ref: Spec: 9.1.1 (1)
+            natsort($value);
+            foreach ($value as $duplicate_value) {
+                $pairs[] = $parameter . '=' . $duplicate_value;
+            }
+        } else {
+            $pairs[] = $parameter . '=' . $value;
+        }
+    }
+    // For each parameter, the name is separated from the corresponding value by an '=' character (ASCII code 61)
+    // Each name-value pair is separated by an '&' character (ASCII code 38)
+    return implode('&', $pairs);
 }
 
-function urlencode_rfc3986($input) {
-  if (is_array($input)) {
-    return array_map('urlencode_rfc3986', $input);
-  } else if (is_scalar($input)) {
-    return str_replace(
-      '+',
-      ' ',
-      str_replace('%7E', '~', rawurlencode($input))
+function urlencode_rfc3986($input)
+{
+    if (is_array($input)) {
+        return array_map('urlencode_rfc3986', $input);
+    } elseif (is_scalar($input)) {
+        return str_replace(
+        '+',
+        ' ',
+        str_replace('%7E', '~', rawurlencode($input))
     );
-  } else {
-    return '';
-  }
+    } else {
+        return '';
+    }
 }
